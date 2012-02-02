93edbf527aa5ebfb653fdb77ec17141cbd6a2e6e
Giel Berkers <info@gielberkers.com>
2012-02-02 22:10:50 +0100

    final adjustments
    - removed paypal
    - added tweet button
    - some CSS tweaks
    - clicking on the code selects it

--- compass/config.rb
+++ compass/config.rb
@@ -7,7 +7,7 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
-output_style = :nested
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
--- js/httool.js
+++ js/httool.js
@@ -73,3 +73,29 @@ for(var i = 0, l = elements.length; i < l; i++)
     elements[i].addEventListener('change', update);
 }
 
+output.addEventListener('click', function(e){
+    e.preventDefault();
+    fnSelect(output);
+});
+
+function fnSelect(obj) {
+    fnDeSelect();
+    if (document.selection) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(obj);
+        range.select();
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(obj);
+        window.getSelection().addRange(range);
+    }
+}
+
+function fnDeSelect() {
+    if (document.selection)
+    {
+        document.selection.empty();
+    } else if (window.getSelection) {
+        window.getSelection().removeAllRanges();
+    }
+}
